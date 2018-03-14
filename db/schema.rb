2e044f5b7f6dd30f358683ea9c15e3544348e430
root <root@DESKTOP-FJJP9AR.localdomain>
2018-03-14 18:38:40 +0900

    migrate add_ pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314085314) do
+ActiveRecord::Schema.define(version: 20180314093703) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180314085314) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
