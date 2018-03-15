b5ef9007c04fb2b5c78290b6e24eed341eec5ddf
root <root@DESKTOP-FJJP9AR.localdomain>
2018-03-15 17:30:23 +0900

    after migrate and make a select form of language

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314093703) do
+ActiveRecord::Schema.define(version: 20180315074454) do
+
+  create_table "languages", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "language"
+    t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
