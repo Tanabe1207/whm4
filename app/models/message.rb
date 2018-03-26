c4eec6c778cab8b6dbcd1db557e6180bc1095393
root <root@DESKTOP-FJJP9AR.localdomain>
2018-03-26 16:31:26 +0900

    massage done except for appearing message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+
+  after_create_commit{MessageBroadcastjob.perform_later self}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319085336) do
+ActiveRecord::Schema.define(version: 20180325094216) do
 
   create_table "languages", force: :cascade do |t|
     t.integer "user_id"
     t.string "language"
     t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
